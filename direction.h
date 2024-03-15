12bb850de037b0c27036e245fc3ff8a205025502
brooklvk <brooklvk@byui.edu>
2024-03-15 17:52:48 -0600

    added define m_pi and m_pi_2

--- direction.h
+++ direction.h
@@ -8,6 +8,12 @@
 #ifndef Direction_h
 #define Direction_h
 
+#ifndef M_PI
+#define M_PI    3.14159265358979323846
+#define M_PI_2  1.57079632679489661923
+#endif // M_PI 
+
+
 class Direction {
 private:
     double radians;  // Private attribute for storing radians
