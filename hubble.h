dce3a21871adee9311a8b2a3aff7ce022f14efa8
Spencer Lamoreaux <lam21016@byui.edu>
2024-03-29 13:46:43 -0600

    drawing hubble

--- hubble.h
+++ hubble.h
@@ -1,6 +1,15 @@
 
 #pragma once
 #include "whole.h"
+#include "uiDraw.h"
 
 class Hubble : public Whole {
+public:
+    Hubble(){};
+    Hubble(double x, double y){
+        pos = Position(x, y);
+    };
+    virtual void draw() override{
+        ogstream().drawHubble(pos, angularVelocity);
+    }
 };
